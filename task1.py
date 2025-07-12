8f701d9c0f3d194f6fcade6ade3017389712b8a2
Ankitbaghel07 <ankitbaghel00007@gmail.com>
2025-07-12 17:50:53 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#add,sub,divide,multiply
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
---
+++ task2.py
@@ -0,0 +1,4 @@
+#greeting using user name
+a=str(input("enter your first name:"))
+b=str(input("enter your last name:"))
+print(f"hello,{a} {b}! welcome to python program.")
